0418f2d648c23360bfa2963f32673e31d80b8cb9
Rahul <patelrahul3501@gmail.com>
2026-03-09 13:36:04 +0530

    first commit to develop branch

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ console.log("Hello World");
 // 1 -> git --version
         // checks version of git
 
+// 2 -> git config --global user.name "rahul"
+        // set the name of the user
+
+// 3 -> git config --global user.email "0K4dI@example.com"
+        // set the email of the user
+
+// 4 -> git init
+        // initialize the git   
+
+// 5 -> git status
+        // checks the status of git
+
